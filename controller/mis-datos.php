8b76754e1710ec9921e55a0904b5282bbeb6c4ec
Diegui77o <gonzalez_diego@outlook.com>
2017-10-05 11:23:57 -0300

    Inicio modulo mis datos + cambios minimos
    Controlador y vista del modulo mis datos. Baja del producto y algunos cambios minimos.

---
+++ controller/mis-datos.php
@@ -0,0 +1,37 @@
+<?php
+
+// Controlador que muestra los datos del usuario logueado en el sistema
+require_once 'cargaTwig.php';
+
+session_start();
+if (isset($_SESSION["rol"])) {
+    $rol = $_SESSION["rol"];
+    if (($rol == 1) or ($rol == 2) or ($rol == 3)) {
+        $idusuario = $_SESSION["id"];
+        include '../model/usuario.php';
+        $usuario           = new Usuario();
+        $usuarioEncontrado = $usuario->buscarUsuario($idusuario);
+        // Cargo la vista
+        echo $twig->render('layout/mis-datos.html.twig', array(
+            'pagina'            => ' - Mis datos',
+            'rol'               => $rol,
+            'usuarioEncontrado' => $usuarioEncontrado,
+        ));
+    } else {
+        // Cargo la vista error permisos
+        echo $twig->render('layout/mensaje.html.twig', array(
+            'pagina'      => ' - Mensaje del sistema',
+            'rol'         => $rol,
+            'tipoDePanel' => 'panel-danger',
+            'msj'         => 'Error! No posee los permisos necesarios para realizar está operación.',
+        ));
+    }
+} else {
+    // Cargo la vista error de sesion
+    echo $twig->render('layout/mensaje.html.twig', array(
+        'pagina'      => ' - Mensaje del sistema',
+        'rol'         => $rol,
+        'tipoDePanel' => 'panel-danger',
+        'msj'         => 'Error! No posee sesion activa, por favor vuelva a ingresar al sistema.',
+    ));
+}
---
+++ controller/producto-delete.php
@@ -0,0 +1,54 @@
+<?php
+
+// Controller que elimina un producto de la base de datos
+require_once 'cargaTwig.php';
+
+session_start();
+if (isset($_SESSION["rol"])) {
+    $rol = $_SESSION["rol"];
+    if ($rol == 1) {
+        // id del producto enviado por el formulario
+        $id = $_POST["id"];
+        include "../model/producto.php";
+        $producto = new Producto();
+        $producto->eliminarProducto($id);
+        $buscarProducto     = $producto->buscarProducto($id);
+        $listadoDeProductos = $producto->listarProductos();
+        // Si cuando busco el id no se encuentra es porque fue eliminado
+        if ($buscarProducto == null) {
+            // Cargo la vista
+            echo $twig->render('producto/listado.html.twig', array(
+                'pagina'             => ' - Listado de productos',
+                'rol'                => $rol,
+                'msjExito'           => true,
+                'msj'                => 'El producto fue eliminado correctamente!',
+                'listadoDeProductos' => $listadoDeProductos,
+            ));
+        } else {
+            // Cargo la vista
+            echo $twig->render('producto/listado.html.twig', array(
+                'pagina'             => ' - Error',
+                'rol'                => $rol,
+                'msjError'           => true,
+                'msj'                => 'El producto no puede ser eliminado porque posee referencias en otras tablas.',
+                'listadoDeProductos' => $listadoDeProductos,
+            ));
+        }
+    } else {
+        // Cargo la vista error permisos
+        echo $twig->render('layout/mensaje.html.twig', array(
+            'pagina'      => ' - Mensaje del sistema',
+            'rol'         => $rol,
+            'tipoDePanel' => 'panel-danger',
+            'msj'         => 'Error! No posee los permisos necesarios para realizar está operación.',
+        ));
+    }
+} else {
+    // Cargo la vista error de sesion
+    echo $twig->render('layout/mensaje.html.twig', array(
+        'pagina'      => ' - Mensaje del sistema',
+        'rol'         => $rol,
+        'tipoDePanel' => 'panel-danger',
+        'msj'         => 'Error! No posee sesion activa, por favor vuelva a ingresar al sistema.',
+    ));
+}
