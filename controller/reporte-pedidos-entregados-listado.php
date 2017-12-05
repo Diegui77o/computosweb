38299109f28d15e3e8e9adffc3ca9b58ad318e97
Diegui77o <gonzalez_diego@outlook.com>
2017-12-05 07:43:54 -0300

    Correcciones minimas
    Correcciones minimas en reportes.

--- controller/reporte-pedidos-entregados-listado.php
+++ controller/reporte-pedidos-entregados-listado.php
@@ -1,34 +1,33 @@
-<?php 
+<?php
 // Controlador que imprime el listado de los pedidos entregados
 require_once 'cargaTwig.php';
 session_start();
-if (isset($_SESSION["rol"])){
-	$rol = $_SESSION["rol"];
-	if ($rol == 1){
-		include "../model/pedido-detalle.php";
-		$pedidoDetalle = new PedidoDetalle();
-		$listadoDePedidos = $pedidoDetalle->listarPedidosEntregados();
-		// Creacion del objeto de la clase heredada
-		include '../controller/reporte-plantilla-pedidos-entregados.php';
-		$pdf = new PDF();
-		$pdf->SetTopMargin ( 5.4 );
-		$pdf->SetLeftMargin ( 4.5 );
-		$pdf->AliasNbPages ();
-		$pdf->SetFont ( 'Times', 'I', 9 );
+if (isset($_SESSION["rol"])) {
+    $rol = $_SESSION["rol"];
+    if ($rol == 1) {
+        include "../model/pedido-detalle.php";
+        $pedidoDetalle    = new PedidoDetalle();
+        $listadoDePedidos = $pedidoDetalle->listarPedidosEntregados();
+        // Creacion del objeto de la clase heredada
+        include '../controller/reporte-plantilla-pedidos-entregados.php';
+        $pdf = new PDF();
+        $pdf->SetTopMargin(5.4);
+        $pdf->SetLeftMargin(4.5);
+        $pdf->AliasNbPages();
+        $pdf->SetFont('Times', 'I', 9);
 
+        $pdf->AddPage();
+        foreach ($listadoDePedidos as $fila => $valor) {
+            $pdf->Text(5, $pdf->GetY(), utf8_decode($listadoDePedidos[$fila][11]) . ", " . utf8_decode($listadoDePedidos[$fila][10]));
+            $pdf->Text(48, $pdf->GetY(), utf8_decode($listadoDePedidos[$fila][15]));
+            $pdf->Text(95, $pdf->GetY(), utf8_decode($listadoDePedidos[$fila][4] . " (" . $listadoDePedidos[$fila][5]) . ")");
+            $pdf->Text(165, $pdf->GetY(), utf8_decode($listadoDePedidos[$fila][3]));
+            $pdf->Text(188, $pdf->GetY(), utf8_decode(date("d-m-Y", strtotime($listadoDePedidos[$fila][6]))));
+            $pdf->cell(0, 5.5, '', 0, 1);
+        }
 
-		$pdf->AddPage ();
-		foreach ( $listadoDePedidos as $fila => $valor ) {
-			$pdf->Text ( 5, $pdf->GetY (), utf8_decode ( $listadoDePedidos [$fila] [11]).", " . utf8_decode ($listadoDePedidos [$fila] [10] ));
-			$pdf->Text ( 52, $pdf->GetY (), utf8_decode ( $listadoDePedidos [$fila] [15] ));
-			$pdf->Text(95, $pdf->GetY(), utf8_decode($listadoDePedidos[$fila][4] . " (" . $listadoDePedidos[$fila][5]) . ")");
-			$pdf->Text ( 165, $pdf->GetY (), utf8_decode ( $listadoDePedidos [$fila] [3] ));
-			$pdf->Text ( 188, $pdf->GetY (), utf8_decode ( date("d-m-Y", strtotime($listadoDePedidos [$fila] [6])) ));
-			$pdf->cell ( 0, 5.5, '', 0, 1 );
-		}
-
-		$pdf->Output ();
-	} else {
+        $pdf->Output();
+    } else {
         // Cargo la vista error permisos
         echo $twig->render('layout/mensaje.html.twig', array(
             'pagina'      => ' - Mensaje del sistema',
